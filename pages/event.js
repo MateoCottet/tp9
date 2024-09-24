65321041a76abcc17c3032eb411000a312cb12f6
MATEO COTTET <47168146@ort>
2024-09-24 08:31:44 -0300

    tp 10

--- pages/event.js
+++ pages/event.js
@@ -3,7 +3,7 @@ export default function EventDetail() {
     return (
       <div className="event-detail">
         <h1>Event Title</h1>
-        <p>Event details go here...</p>
+        <p>Event details go here</p>
       </div>
     );
   }
